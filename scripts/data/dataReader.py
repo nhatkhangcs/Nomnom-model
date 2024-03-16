306067f3903602f15d8ef7a83dd349df4b388240
Khang Vo <nhatkhangdtp@gmail.com>
2024-03-16 11:41:29 +0700

    feat: searching based on query and tags

--- scripts/data/dataReader.py
+++ scripts/data/dataReader.py
@@ -1,45 +1,53 @@
+# Import the pickle library
 import pandas as pd
+import pickle
+import os
 
 class DataReader:
     def __init__(self, data_path):
         self.data_path = data_path
-        self.data = None
+        self.data = pd.read_csv(data_path)
 
-    def load_data_for_embeddings(self, key):
+    def load_data_embed_name(self):
         '''
             return: pandas dataframe
         '''
-        print('[+] Loading data into DataReader...')
-        data = pd.read_csv(self.data_path)
-        return data[key]
+        
+        return self.data['name']
     
     def dataframe_creator(self):
         '''
             return: list of items <food_id, name, picture_url, rating, calories, sugar, podium, protein, saturated_fat> 
         '''
-        data = pd.read_csv(self.data_path)
-        return data
+        print('[+] Loading data into DataReader...')
+        if os.path.exists('data/recipe_list.pkl'):
+            with open('data/recipe_list.pkl', 'rb') as f:
+                recipe_list = pickle.load(f)
+                return recipe_list
+        # Clean and split tags using list comprehensions
+        df_process = self.data.copy()
+        df_process['tags'] = df_process['tags'].apply(lambda tags: [tag.strip()[1:-1] for tag in tags.strip()[1:-1].split(',') if tag.strip()])
+
+        # Define the template for applying to the 'nutrition' column
+        df_process['nutrition'] = df_process['nutrition'].apply(lambda nutrition: [float(num) for num in nutrition.strip('[]').split(',')])
+        recipe_list = []
+        for index, row in df_process.iterrows():
+            recipe_list.append((row['name'], row['id'], row['tags'], row['nutrition'], row['description']))
+
+        # Open a file for writing in binary mode
+        with open('data/recipe_list.pkl', 'wb') as f:
+            # Pickle the recipe_list object
+            pickle.dump(recipe_list, f)
+        return recipe_list
     
 if __name__ == '__main__':
     list_items = []
     # each item is a dictionary, each dictionary has the following keys: food_id, name, picture_url, rating, calories, sugar, podium, protein, saturated_fat
     data_reader = DataReader('data/RAW_recipes.csv')
-    #data = data_reader.load_data_for_embeddings('name')
+    #data = data_reader.load_data_embed_name()
     #print(data)
     # print row names
     #print(data_reader.dataframe_creator().columns)
     #print(data_reader.dataframe_creator().iloc[[0]])
 
-    for i in range(1):
-        dict_item = {}
-        dict_item['food_id'] = data_reader.dataframe_creator().iloc[i]['id']
-        dict_item['name'] = data_reader.dataframe_creator().iloc[i]['name']
-        # dict_item['picture_url'] = data_reader.dataframe_creator().iloc[i]['picture_url']
-        # dict_item['rating'] = data_reader.dataframe_creator().iloc[i]['average_rating']
-        dict_item['description'] = data_reader.dataframe_creator().iloc[i]['description']
-        dict_item['nutrition'] = data_reader.dataframe_creator().iloc[i]['nutrition']
-        dict_item['tags'] = data_reader.dataframe_creator().iloc[i]['tags']
-        # dict_item['votes'] = data_reader.dataframe_creator().iloc[i]['votes']
-        list_items.append(dict_item)
-
-    print(list_items)
+    
